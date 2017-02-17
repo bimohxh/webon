a5fdc23f84e073f9b695ac7fe92c4aa644f74eff
hxh <1246996371@qq.com>
2017-02-17 20:07:28 +0800

    more

--- bin/webon.js
+++ bin/webon.js
@@ -4,6 +4,7 @@ const program = require('commander')
 const Helper = require('../lib/helper')
 const Init = require('../lib/init')
 const Server = require('../lib/server')
+const Oss = require('../lib/oss')
 const colors = require('colors')
 
 
@@ -12,10 +13,12 @@ program
   .usage('[命令] [参数]')
 
 program
-  .command('start')
+  .command('s')
   .description('启动静态文件服务器')
+  .option('-p, --port', '指定端口，将会覆盖配置文件中的 port 配置')
   .action(() => {
-    Server.start()
+    let args = Helper.cmdArg(program.rawArgs)
+    Server.start(args.p)
   })
 
 program
@@ -32,44 +35,17 @@ program
     Init.viewConfig()
   })
 
-// program
-//   .command('copygoods')
-//   .description('复制商品')
-//   .option('-f, --from', '从该经销商或公司复制所有商品，公司为 c（company） 开头，经销商以 d（delaer） 开头，商品以 g（goods） 开头')
-//   .option('-t, --to', '复制给哪个经销商或公司，公司为 c（company） 开头，经销商以 d（delaer） 开头')
-//   .action(() => {
-//     let args = Helper.cmdArg(program.rawArgs)
-
-//     if (!Helper.hasLocalEnv()) {
-//       Helper.msg('配置文件不存在，请执行 dpjgo init 进行配置', 'error')
-//       return
-//     }
-
-//     if (!args.t && !args.f) {
-//       program.commands[0].help()
-//       return
-//     }
-
-//     if (!args.f) {
-//       Helper.msg('你要复制哪个经销商或公司的商品，别忘了 -f 参数', 'error')
-//       return
-//     }
-
-//     if (!args.t) {
-//       Helper.msg('你要复制给哪个经销商或公司，别忘了 -t 参数', 'error')
-//       return
-//     }
-
-//     Goods.copy(args.t, args.f)
-//   })
-//   .on('--help', () => {
-//     console.log('  示例:')
-//     console.log('')
-//     console.log('    $ dpjgo copygoods -f c2 -t d3'.blue)
-//     console.log('    $ dpjgo copygoods -f g2,g3 -t c4'.blue)
-//     console.log('')
-//   })
-
+program
+  .command('deploy')
+  .description('部署当前文件到云存储')
+  .option('-d, --dist', '选择目标云存储，目前支持 qiniu。将覆盖配置文件中的 oss 配置')
+  .action(() => {
+    if (!Helper.hasLocalEnv()) {
+      Helper.msg('配置文件不存在，请运行 webon init', 'error')
+      return
+    }
+    Oss.deploy()
+  })
 
 
 
